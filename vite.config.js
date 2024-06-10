7349ac237f717254ea6923d978907145d720161b
Santiago Curotto <santiago.curotto.98@gmail.com>
2024-06-10 20:01:36 -0300

    arreglos para netifly parte 12

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-        'sweetalert2',
-        'sweetalert2/dist/sweetalert2.min.css' // Añade esta línea
-      ]
+      external: ['history', 'sweetalert2', 'sweetalert2/dist/sweetalert2.min.css' ]
+      
     }
   }
-});
+})
